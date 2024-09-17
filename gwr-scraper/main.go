c5d860f56a263c8bde539b943766f8de75c40d5b
ewright3 <eugene@ewright3.com>
2024-09-17 18:40:56 -0400

    new scraper using scrapethissite.com

--- gwr-scraper/main.go
+++ gwr-scraper/main.go
@@ -12,17 +12,26 @@ func main() {
 		colly.AllowedDomains("scrapethissite.com", "www.scrapethissite.com"),
 	)
 
-	c.OnHTML(".col-md-4.country", func(e *colly.HTMLElement) {
+	c.OnHTML(".team", func(e *colly.HTMLElement) {
+		if strings.Contains(strings.ToLower(e.ChildText(".name")), "capitals") {
+			fmt.Println(strings.TrimSpace(
+				e.ChildText(".name")),
+				strings.TrimSpace(e.ChildText(".year")),
+				strings.TrimSpace(e.ChildText(".pct")))
+		}
 
-		e.ChildText(".country-name")
-		fmt.Println(strings.TrimSpace(
-			e.ChildText(".country-name")),
-			strings.TrimSpace(e.ChildText(".country-capital")))
+	})
 
+	c.OnHTML(".pagination", func(e *colly.HTMLElement) {
+		currentPage := e.ChildText("strong")
+		lastPage := e.ChildText("li:nth-last-child(2) > a")
+		fmt.Println("page number:", strings.TrimSpace(currentPage))
+		fmt.Println("last page number:", strings.TrimSpace(lastPage))
 	})
 
 	c.OnRequest(func(request *colly.Request) {
 		fmt.Println("Visiting", request.URL.String())
+
 	})
-	c.Visit("https://www.scrapethissite.com/pages/simple/")
+	c.Visit("https://www.scrapethissite.com/pages/forms/?page_num=20")
 }
