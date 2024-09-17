b5dce097fa1675fc3bba2501dd5888934ccfb9e5
ewright3 <eugene@ewright3.com>
2024-09-17 16:39:10 -0400

    test scraper for scrapethissite.com/pages/simple

--- gwr-scraper/main.go
+++ gwr-scraper/main.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
 
 func main() {
-	c := colly.NewCollector()
-	fmt.Println("Visiting", c)
+	c := colly.NewCollector(
+		colly.AllowedDomains("scrapethissite.com", "www.scrapethissite.com"),
+	)
+
+	c.OnHTML(".col-md-4.country", func(e *colly.HTMLElement) {
+
+		e.ChildText(".country-name")
+		fmt.Println(strings.TrimSpace(
+			e.ChildText(".country-name")),
+			strings.TrimSpace(e.ChildText(".country-capital")))
+
+	})
+
+	c.OnRequest(func(request *colly.Request) {
+		fmt.Println("Visiting", request.URL.String())
+	})
+	c.Visit("https://www.scrapethissite.com/pages/simple/")
 }
