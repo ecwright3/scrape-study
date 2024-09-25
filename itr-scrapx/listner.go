a083d3ccdd2cefdecc2099136f168222feb9fa07
Gene Wright <ecwright3@gmail.com>
2024-09-25 19:14:31 -0400

    added listned

---
+++ itr-scrapx/listner.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func startListner() {
+	//Create New Router
+	r := mux.NewRouter()
+
+	//Define a route
+	r.Path("/mone/").Queries("target", "{key}").HandlerFunc(HomeHandler).Name("HomeHandler")
+	//r.HandleFunc("/{name}", HomeHandler)
+	//http.Handle("/", r)
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	target := r.FormValue("target")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Name: %v\n", target)
+}
