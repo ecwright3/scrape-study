2035e2616aa60d3e60bb0b3cda6b346d32df3eaf
ewright3 <eugene@ewright3.com>
2024-09-25 19:30:40 -0400

    update main url

--- itr-scrapx/main.go
+++ itr-scrapx/main.go
@@ -1,9 +1,20 @@
 package main
 
-var sampleUrl string
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	sampleUrl = ""
 
-	getImages(sampleUrl)
+	var url string
+
+	if len(os.Args) > 1 {
+		url = os.Args[1]
+	} else {
+		fmt.Println("Enter Target URL:")
+		fmt.Scan(&url)
+	}
+
+	getImages(url)
 }
